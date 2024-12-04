6bf6dee63fa7357a763dbe199d5b5c2d5e957916
Khadim1511 <89653168+Khadim1511@users.noreply.github.com>
2024-12-04 21:43:59 +0100

    app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import os
 import uuid
 
 
-DATABASE_URL = "postgresql://recen_xl1y_user:9VE7TJGshNIkqYFzTEzFS95bLXsL1ZDj@dpg-ct84e4pu0jms73aun6p0-a.oregon-postgres.render.com/recen_xl1y"
 def get_db_connection():
     conn = psycopg2.connect(
         dbname='recen',
         user='recen_xl1y_user',
         password='9VE7TJGshNIkqYFzTEzFS95bLXsL1ZDj',
-        host='dpg-ct84e4pu0jms73aun6p0-a',
+        host='dpg-ct84e4pu0jms73aun6p0-a.oregon-postgres.render.com',
         port='5432'  # Port par défaut de PostgreSQL
 
     )
